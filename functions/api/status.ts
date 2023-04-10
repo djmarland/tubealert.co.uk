9370ae31bedbc19bd88f2512f3467bec464504af
David Marland <davidm@rland.me.uk>
2023-04-10 17:47:55 +0100

    Test fetching data

--- functions/api/status.ts
+++ functions/api/status.ts
@@ -1,3 +1,22 @@
-export function onRequest(context) {
-  return new Response("Hello, world!");
+const fetchData = async (appID, appKey) => {
+  const url =
+    "https://api.tfl.gov.uk/Line/Mode/tube,dlr,elizabeth-line,overground/Status" +
+    `?app_id=${appID}` +
+    `&app_key=${appKey}`;
+
+  const data = await fetch(url).then((d) => d.json);
+  return JSON.stringify(data);
+};
+
+interface Env {
+  TFL_APP_ID: string;
+  TFL_APP_KEY: string;
+  // KV: KVNamespace;
 }
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  // const value = await context.env.KV.get('example');
+  return new Response(
+    await fetchData(context.env.TFL_APP_ID, context.env.TFL_APP_KEY)
+  );
+};
