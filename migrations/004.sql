5203bc7583637dc65763dc954a494862ac1899ff
David Marland <davidm@rland.me.uk>
2026-09-01 15:42:07 +0100

    Database Improvements
    * Fix Ampersands
    * Indexes to reduce row counts

---
+++ migrations/004.sql
@@ -0,0 +1,20 @@
+CREATE INDEX IF NOT EXISTS idx_subscription_line_day_hour_is_block_start
+    ON subscription(line_id, day, hour, is_block_start);
+
+CREATE INDEX IF NOT EXISTS idx_subscription_subscriber_line
+    ON subscription(subscriber_id, line_id);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_subscription_unique_subscriber_line_day_hour
+    ON subscription(subscriber_id, line_id, day, hour);
+
+UPDATE `line`
+    SET
+        `name` = 'Hammersmith & City Line',
+        `short_name` = 'Hammersmith & City'
+  WHERE `id` = 'hammersmith-city';
+
+UPDATE `line`
+    SET
+        `name` = 'Waterloo & City Line',
+        `short_name` = 'Waterloo & City'
+  WHERE `id` = 'waterloo-city';
